6ede5af95e5e56a63f9ec63ab54db19a169dc98c
sangnd-fabbi <55520622+sangnd-fabbi@users.noreply.github.com>
2021-04-25 11:21:40 +0700

    First commit

--- config/settings.py
+++ config/settings.py
@@ -121,5 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-X_FRAME_OPTIONS = 'aapie-ec3.myshopify.com'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+X_FRAME_OPTIONS = os.environ.get('X_FRAME_OPTIONS', 'aapie-ec3.myshopify.com')
